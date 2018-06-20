41355f605d2f4fe515cd113aa04373092605602f
Thiago Contardi <thiagocontardi@hotmail.com>
2018-06-20 15:43:15 -0300

    Added Helper

---
+++ app/code/community/Contardi/ProductList/Helper/Data.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Interface
+ *
+ * @category   Contardi
+ * @package    Contardi_ProductList
+ * @author     Thiago Contardi (thiago@contardi.com.br)
+ * @copyright  Copyright (c) 2018
+ */
+class Contardi_ProductList_Helper_Data
+{
+
+}
